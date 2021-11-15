8506152d57b1c980e592b6741b603ba0a4ac32a6
magnetoip <magnetoazure@gmail.com>
2021-11-15 15:37:54 +0530

    New changes

---
+++ src/main/java/com/celcom/portal/configserver/InstallerPortalConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.celcom.portal.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class InstallerPortalConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstallerPortalConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/celcom/portal/configserver/InstallerPortalConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celcom.portal.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstallerPortalConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
